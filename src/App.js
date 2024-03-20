07eb07b549cdebaffab864a6f7e961d4e53b591c
Jayan Vinod <jayan.vinod@elastic.run>
2024-03-20 11:50:08 +0530

    Task Bar Added

--- src/App.js
+++ src/App.js
@@ -5,18 +5,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. and test it Jayan Vinod
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="App-header-name montserrat">
+          <p style={{marginTop: 50,marginLeft:80}}>Jayan Vinod</p>
+        </div>
+        <div style={{paddingRight:80}} className="App-header-options">
+          <p style={{marginTop: 50,marginLeft:40}}>About Me</p>
+          <p style={{marginTop: 50,marginLeft:40}}>Services</p>
+          <p style={{marginTop: 50,marginLeft:40}}>Projects</p>
+          <p style={{marginTop: 50,marginLeft:40}}>References</p>
+          <div style={{marginTop: 40,marginLeft:40}} className='resume-button'>Resume</div>
+        </div>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
       </header>
     </div>
   );
